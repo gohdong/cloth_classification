6d090c19e95fb41d2618d3ba6bf0f09238362f5d
gohdong <aldehf420@naver.com>
2022-02-15 09:25:26 +0900

    model2

--- run_tf_model/test.js
+++ run_tf_model/test.js
@@ -1,21 +1,20 @@
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 
-let classes = fs.readFileSync('./model/dict.txt','utf8').split('\n');
+let classes = fs.readFileSync('./model2/dict.txt','utf8').split('\n');
 
 const excuteModel = async () => {
-	const model = await tf.loadGraphModel('file://./model/model.json');
+	const model = await tf.loadGraphModel('file://./model2/model.json');
 	let img = processImage('./ex2.png')
 	const prediction = await model.predict(img).dataSync();
 	printResult(prediction);
-
 }
 
 function processImage(path) {
 	const imageSize = 224
 	const imageBuffer = fs.readFileSync(path); // can also use the async readFile instead
 	// get tensor out of the buffer
-	image = tf.node.decodeImage(imageBuffer, 3);
+	image = tf.node.decodeImage(imageBuffer,3);
 	// dtype to float
 	image = image.cast('float32').div(255);
 	// resize the image
@@ -29,6 +28,6 @@ function printResult(data){
 		console.log(`${classes[index]} : ${element}`);
 	}
 }
-
+console.log(classes);
 excuteModel()
 
