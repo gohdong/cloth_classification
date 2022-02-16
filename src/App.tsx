b97de0f674959c9ee6e51b8c636d099fed195d44
gohdong <aldehf420@naver.com>
2022-02-16 22:44:27 +0900

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import React from "react";
 import "./App.css";
 import AppBar from "./appbar/AppBar";
+import LeftSidebar from "./left_sidebar/LeftSidebar";
+import RightSideBar from "./right_sidebar/RightSideBar";
 
 function App() {
 	return (
 		<>
+			<AppBar/>
 			<div id="main-wrapper">
-				<AppBar/>
 				<div id="contents-wrapper">
 					<div id="static-contents">
 						<div className="flex-wrapper">
-							<div id="left-sidebar">
-								<p>drag or add items</p>
-								<div id="left-sidebar-buttons">
-									<p>+</p>
-									<p>Clear All</p>
+							<LeftSidebar/>
+							<div id="main-contents">
+								<div id="placeholder">
+									<div className="" />
 								</div>
 							</div>
-							<div id="main-contents">
-
-							</div>
-						</div>
-						<div id="bottom-status">
-
 						</div>
 					</div>
 					<div id="toggle-contents">
-						<div id="right-sidebar">
-
-						</div>
+						<RightSideBar/>
 					</div>
+				</div>
+				<div id="bottom-status">
+					status
 				</div>
 			</div>
 		</>
---
+++ src/left_sidebar/LeftSidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LeftSidebar() {
+	return (
+		<div id="left-sidebar">
+			<p>drag or add items</p>
+			<div id="left-sidebar-buttons">
+				<p>+</p>
+				<p>Clear All</p>
+			</div>
+		</div>
+	);
+}
