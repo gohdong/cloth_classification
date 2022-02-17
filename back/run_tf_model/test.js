a1802ed613b36ab3142780b6daba2ed76c6bbfb1
WangJongHwi <wangjonghwi@WangJongHwiui-MacBookAir.local>
2022-02-17 23:21:22 +0900

    back server

--- back/run_tf_model/test.js
+++ back/run_tf_model/test.js
@@ -1,20 +1,20 @@
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
 
-let classes = fs.readFileSync('./model2/dict.txt','utf8').split('\n');
+let classes = fs.readFileSync('./run_tf_model/model2/dict.txt','utf8').split('\n');
 
-const excuteModel = async () => {
-	const model = await tf.loadGraphModel('file://./model2/model.json');
-	let img = processImage('./ex2.png')
+exports.excuteModel = async (imgPath) => {
+	const model = await tf.loadGraphModel('file://./run_tf_model/model2/model.json');
+	let img = processImage(imgPath)
 	const prediction = await model.predict(img).dataSync();
-	printResult(prediction);
+	return toJson(prediction)
 }
 
 function processImage(path) {
 	const imageSize = 224
 	const imageBuffer = fs.readFileSync(path); // can also use the async readFile instead
 	// get tensor out of the buffer
-	image = tf.node.decodeImage(imageBuffer,3);
+	image = tf.node.decodeImage(imageBuffer, 3);
 	// dtype to float
 	image = image.cast('float32').div(255);
 	// resize the image
@@ -23,11 +23,18 @@ function processImage(path) {
 	return image
 }
 
-function printResult(data){
+function toJson(data){
+	var ob = new Object();
+	var arr = new Array();
+
 	for(let [index,element] of data.entries()){
-		console.log(`${classes[index]} : ${element}`);
+		var tmp = Object();
+		tmp.class = classes[index];
+		tmp.name = element;
+		arr.push(tmp);
 	}
+	ob.probs = arr;
+	return ob;
 }
-console.log(classes);
-excuteModel()
+
 
