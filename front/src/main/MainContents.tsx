27e282e330519f46c29e048251cd83babd66385c
WangJongHwi <wangjh789@hanyang.ac.kr>
2022-03-12 16:16:16 +0900

    [왕종휘] 결과 표현 및 수정

--- front/src/main/MainContents.tsx
+++ front/src/main/MainContents.tsx
@@ -1,10 +1,23 @@
 import {useDropzone} from "react-dropzone";
-import {useCallback} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {useRecoilState, useRecoilValue} from "recoil";
 import {Image, imagesSizeState, imageState, selectedItemID} from "../recoil/imageState";
 import "./MainContents.scss";
 
 const hash = require("object-hash");
+
+const clothesTag: Map<string, Array<string>> = new Map();
+
+clothesTag.set("top", ["under", "t-shirt", "hoodie", "sleep", "shirt", "sweater", "waistcoat", "swim"]);
+clothesTag.set("bottom", ["under", "trouser", "sleep", "skirt", "short", "jean", "swim"]);
+clothesTag.set("one", ["under", "dress", "sleep", "jumpsuit", "setup", "swim"]);
+clothesTag.set("outer", ["jacket", "blazer", "coat", "cardigan"]);
+clothesTag.set("shoes", []);
+clothesTag.set("acc", []);
+clothesTag.set("bag", []);
+
+const genderTag: Array<string> = ["male", "unisex", "female"];
+const colorTag: Array<string> = ["black", "white", "blue", "yellow", "brown", "grey", "red", "beige", "orange", "khaki", "turquoise", "green", "purple", "pink"];
 
 export async function onDropHandler(images: Map<string, Image>,
 	setImages: Function, setID: Function, acceptedFiles: File[]) {
@@ -27,7 +40,6 @@ export async function onDropHandler(images: Map<string, Image>,
 			const modelResultTag = new Map();
 			const modelProbs = new Map();
 			const formData = new FormData();
-			let check = false;
 
 			formData.append("img", item);
 
@@ -37,7 +49,6 @@ export async function onDropHandler(images: Map<string, Image>,
 			})
 				.then(response => response.json())
 				.then(data => {
-					console.log(data);
 					for (let i = 0; i < data.probs.length; i++) {
 						modelProbs.set(data.probs[i].class, new Map());
 						if (data.probs[i].class === "shoes" || data.probs[i].class === "acc" || data.probs[i].class === "bag") {
@@ -49,19 +60,16 @@ export async function onDropHandler(images: Map<string, Image>,
 									data.probs[i].subcate[j].value);
 							}
 						}
-
-
 						if (data.probs[i].class === "gender") {
 							modelResultTag.set("gender", data.probs[i].subcate[0].class);
 						} else if (data.probs[i].class === "color") {
 							modelResultTag.set("color", data.probs[i].subcate[0].class);
-						} else if (!check) {
-							modelResultTag.set(data.probs[i].class, data.probs[i].subcate[0].class === null ? "" : data.probs[i].subcate[0].class);
-							check = true;
+						} else if (!modelResultTag.has("main")) {
+							modelResultTag.set("main", data.probs[i].class);
+							modelResultTag.set("sub", data.probs[i].subcate[0].class === null ? "" : data.probs[i].subcate[0].class);
 						}
 					}
-					console.log(modelResultTag);
-					console.log(modelProbs);
+
 					/* 콘솔에 표시 */
 				})
 				.then(() => {
@@ -69,7 +77,7 @@ export async function onDropHandler(images: Map<string, Image>,
 						id: fileID,
 						file: item,
 						edited: false,
-						usersTag: [],
+						usersTag: new Map(modelResultTag),
 						modelResultTag,
 						modelProbs,
 					});
@@ -92,6 +100,11 @@ export default function MainContents() {
 	// eslint-disable-next-line no-unused-vars
 	const [currentItemID, setCurrentItemID] = useRecoilState(selectedItemID);
 
+	const [mainCategory, setMainCategory] = useState("");
+	const [subCategory, setSubCategory] = useState("");
+	const [gender, setGender] = useState("");
+	const [color, setColor] = useState("");
+
 
 	const onDrop = useCallback(acceptedFiles => {
 		onDropHandler(images, setImages, setCurrentItemID, acceptedFiles);
@@ -103,6 +116,43 @@ export default function MainContents() {
 		noClick: imagesCount > 0,
 		onDrop,
 	});
+
+	const onClickMainCategory = (value: string,
+		event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		setMainCategory(value ?? "");
+		setSubCategory("");
+		images.get(currentItemID)?.usersTag.set("main", value ?? "");
+		images.get(currentItemID)?.usersTag.set("sub", "");
+	};
+	const onClickSubCategory = (value: string,
+		event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		setSubCategory(value ?? "");
+		images.get(currentItemID)?.usersTag.set("sub", value);
+	};
+	const onClickGenderCategory = (value: string,
+		event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		setGender(value ?? "");
+		images.get(currentItemID)?.usersTag.set("gender", value);
+	};
+	const onClickColorCategory = (value: string,
+		event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+		setColor(value ?? "");
+		images.get(currentItemID)?.usersTag.set("color", value);
+	};
+
+	/* 처음 불러올때 images.get(currentItemID)?.usersTag.get("main") : undefined -> string 으로 바뀔때 다시 렌더링 하도록*/
+	useEffect(() => {
+		console.log(images.get(currentItemID)?.usersTag.get("main"));
+		setMainCategory(images.get(currentItemID)?.usersTag.get("main") ?? "");
+		setSubCategory(images.get(currentItemID)?.usersTag.get("sub") ?? "");
+		setGender(images.get(currentItemID)?.usersTag.get("gender") ?? "");
+		setColor(images.get(currentItemID)?.usersTag.get("color") ?? "");
+	}, [currentItemID, images]);
+
 
 	return (
 		<div id="main-contents" className={isDragActive ? "drag-on" : ""} {...getRootProps()}>
@@ -118,10 +168,34 @@ export default function MainContents() {
 							</div>
 							<div id="category-area">
 								<ul>
-									<li>대분류</li>
-									<li>소분류</li>
-									<li>성별</li>
-									<li>색상</li>
+									<p>대분류</p>
+									<li>
+										{Array.from(clothesTag.keys()).map(i => (
+											<button className={`tag-btn ${i === mainCategory ? "selected" : ""}`} onClick={e => onClickMainCategory(i ?? "", e)}>{i}</button>
+										))}
+									</li>
+
+									<p>소분류</p>
+									<li>
+										{
+											images.get(currentItemID) !== undefined ?
+												clothesTag.get(mainCategory)?.map(i => (
+													<button className={`tag-btn ${i === subCategory ? "selected" : ""}`} onClick={e => onClickSubCategory(i ?? "", e)} >{i}</button>
+												)) : <></>}
+									</li>
+
+									<p>성별</p>
+									<li>
+										{genderTag.map(i => (
+											<button className={`tag-btn ${i === gender ? "selected" : ""}`} onClick={e => onClickGenderCategory(i ?? "", e)} >{i}</button>
+										))}
+									</li>
+									<p>색상</p>
+									<li>
+										{colorTag.map(i => (
+											<button className={`tag-btn ${i === color ? "selected" : ""}`} onClick={e => onClickColorCategory(i ?? "", e)} >{i}</button>
+										))}
+									</li>
 								</ul>
 							</div>
 						</div> :
