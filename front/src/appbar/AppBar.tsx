f88010105f8c50967678dff3173201fb82a27668
gohdong <aldehf420@naver.com>
2022-03-19 21:18:42 +0900

    [고동현] CSV Export 추가

--- front/src/appbar/AppBar.tsx
+++ front/src/appbar/AppBar.tsx
@@ -1,21 +1,50 @@
-import {useSetRecoilState} from "recoil";
+import {useRecoilValue, useSetRecoilState} from "recoil";
+import {CSVLink} from "react-csv";
+import {Data} from "react-csv/components/CommonPropTypes";
 import "./AppBar.scss";
-import ViewSideBar from "../icons/ViewSideBar";
+import ViewSideBar from "../icons/ViewSideBarIcon";
 import {sidebarState} from "../recoil/sidebarState";
+import ExportIcon from "../icons/ExportIcon";
+import {imageState} from "../recoil/imageState";
 
 function AppBar() {
 	const setSidebarOpen = useSetRecoilState(sidebarState);
+	const images = useRecoilValue(imageState);
 	const onClickButton = () => {
 		setSidebarOpen(prev => !prev);
+	};
+
+
+	const getData = () => {
+		const tempData:Data = [["filename", "mainCategory", "subCategory", "gender", "color"]];
+
+		for (const [, value] of images) {
+			tempData.push([value.file.name, value.usersTag.get("main"), value.usersTag.get("sub"), value.usersTag.get("gender"), value.usersTag.get("color")]);
+		}
+		return tempData;
 	};
 
 	return (
 		<div id={"app-bar"}>
 			<div className="app-bar-wrap">
 				<p>LIKE A BLUE PIG</p>
-				<button onClick={onClickButton}>
-					<ViewSideBar/>
-				</button>
+				<div>
+					<CSVLink
+						data={getData()}
+						filename="cloth_classification.csv"
+						target="_blank"
+					>
+						<button>
+							<ExportIcon/>
+						</button>
+					</CSVLink>
+					{/* <button onClick={onClickExport}>*/}
+					{/*	<ExportIcon/>*/}
+					{/* </button>*/}
+					<button onClick={onClickButton}>
+						<ViewSideBar/>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
