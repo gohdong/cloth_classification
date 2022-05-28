0633a2855bb40c38e54b870d97489f69520fb7a2
wangjh789 <wangjh789@hanyang.ac.kr>
2022-05-29 03:19:13 +0900

    clean node

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,11 @@
 import express from "express";
-import multer from "multer";
-import path from "path";
-import {createConnection} from "mysql"
 import {excuteModel} from "../run_tf_model/test"
 
-const fs = require('fs');
-const cors = require('cors');
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true
-}
+// const cors = require('cors');
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   credentials: true
+// }
 
 class App {
   public application: express.Application;
@@ -17,12 +13,6 @@ class App {
     this.application = express();
   }
 }
-const con = createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Xptmxm1212!@',
-  database: 'clothes_tag'
-});
 
 const app = new App().application;
 app.use(express.json());
@@ -34,16 +24,6 @@ app.get("/", (req: express.Request, res: express.Response) => {
   res.send("server start");
 })
 
-const check = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'checks/');
-    },
-    filename: function (req, file, cb) {
-      cb(null, new Date().valueOf() + path.extname(file.originalname));
-    }
-  }),
-});
 
 // app.post('/check', check.single('img'), async (req, res) => {  //유저가 사진을 보내면 판단후 리턴
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -64,32 +44,17 @@ app.post('/check', async (req, res) => {  //유저가 사진을 보내면 판단
   res.json(tmp);
 });
 
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      cb(null, new Date().valueOf() + path.extname(file.originalname));
-    }
-  }),
-});
-app.post('/upload', upload.single('img'), async (req, res) => {  //유저가 태그를 수정하고 업로드하면 다시 저장
-  res.header("Access-Control-Allow-Origin", "*");
-  console.log(req.file);
-  let file = req.file;
-  console.log(req.body['tags'])
-  con.connect();
-
-  var query = `INSERT INTO upload(img_path,user_id,tags,uploaded_at) VALUES ('${file?.path}',1,'${req.body['tags']}',CURRENT_TIMESTAMP)`
-  console.log(query);
-  con.query(query)
-
-  con.end();
-
-  res.json("Upload img");
-});
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, 'uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, new Date().valueOf() + path.extname(file.originalname));
+//     }
+//   }),
+// });
 
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.listen(4000, () => console.log("start"));
